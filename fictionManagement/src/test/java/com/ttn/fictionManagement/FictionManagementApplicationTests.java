13081c04e4dd64f8ea7d746aa118e40d156ac639
nghianek123 <2051052089nghia@ou.edu.vn>
2024-01-28 00:22:14 +0700

    init project pring boot

---
+++ fictionManagement/src/test/java/com/ttn/fictionManagement/FictionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.fictionManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FictionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
