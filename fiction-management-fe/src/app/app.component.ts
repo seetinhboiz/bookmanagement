5ef06f624dea192880e9d3214b73e2db74b0d8aa
nghianek123 <2051052089nghia@ou.edu.vn>
2024-02-09 23:51:10 +0700

    reset fe project

--- fiction-management-fe/src/app/app.component.ts
+++ fiction-management-fe/src/app/app.component.ts
@@ -1,34 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './component/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, RouterModule],
-  // templateUrl: './app.component.html',
-  template: `
-    <main>
-      <div class="header">
-        <app-header></app-header>
-      </div>
+  imports: [RouterOutlet, HeaderComponent, RouterModule],
+  templateUrl: './app.component.html',
+  // template: `
+  //   <main>
+  //     <div class="header">
+  //       <app-header></app-header>
+  //     </div>
 
-      <a [routerLink]="['/']">
-        <header class="brand-name">
-          <img
-            class="brand-logo"
-            src="https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/blue_repicthousebase_1484336386-1.png"
-            alt="logo"
-            aria-hidden="true"
-          />
-        </header>
-      </a>
-      <section class="content">
-        <router-outlet></router-outlet>
-      </section>
-    </main>
-  `,
+  //     <a [routerLink]="['/']">
+  //       <header class="brand-name">
+  //         <img
+  //           class="brand-logo"
+  //           src="https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/blue_repicthousebase_1484336386-1.png"
+  //           alt="logo"
+  //           aria-hidden="true"
+  //         />
+  //       </header>
+  //     </a>
+  //     <section class="content">
+  //       <router-outlet></router-outlet>
+  //     </section>
+  //   </main>
+  // `,
   styleUrl: './app.component.css',
 })
 export class AppComponent {
